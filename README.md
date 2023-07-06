a82d2ad7c8a66b502cad1d31d8b8576a9e5f293b
Igor Pamplona <hayflick@gmail.com>
2023-07-06 14:45:04 +0100

    project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Twitterish Frontend Challenge
+# Twitterish Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'twitterish-frontend-challenge'`, () => {
+  it(`should have as title 'twitterish-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('twitterish-frontend-challenge');
+    expect(app.title).toEqual('twitterish-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('twitterish-frontend-challenge app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('twitterish-frontend app is running!');
   });
 });
